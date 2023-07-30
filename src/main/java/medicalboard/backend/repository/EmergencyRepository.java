628d45f3bdc5aa2fc67a2bc1bdda1ccd03c2a1c1
unknown <lerle144@gmail.com>
2023-07-30 15:02:04 +0900

    [feat] api 추가 - 회원가입, 로그인, 회원정보 조회

--- src/main/java/medicalboard/backend/repository/EmergencyRepository.java
+++ src/main/java/medicalboard/backend/repository/EmergencyRepository.java
@@ -4,4 +4,6 @@ import medicalboard.backend.model.Emergency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmergencyRepository extends JpaRepository<Emergency, Integer>{
+
+
 }
