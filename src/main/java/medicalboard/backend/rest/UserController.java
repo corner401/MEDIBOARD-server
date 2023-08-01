97bbb1879bfb9b2ebb21d85d2662ea7e6e07436f
unknown <lerle144@gmail.com>
2023-08-01 19:40:13 +0900

    누락 수정

--- src/main/java/medicalboard/backend/rest/UserController.java
+++ src/main/java/medicalboard/backend/rest/UserController.java
@@ -2,7 +2,6 @@ package medicalboard.backend.rest;
 
 import medicalboard.backend.DTO.UserDTO;
 import medicalboard.backend.Mapper.UserMapper;
-import medicalboard.backend.entity.User;
 import medicalboard.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 public class UserController {
